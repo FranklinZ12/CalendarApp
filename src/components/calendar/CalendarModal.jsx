bec2152bd3a0d0e00f477e95e51e72d53f613f3d
FranklinZ12 <franklinq8@gmail.com>
2022-03-27 21:27:04 -0500

    validar formulario del modal y obtener informacion del form

--- src/components/calendar/CalendarModal.jsx
+++ src/components/calendar/CalendarModal.jsx
@@ -4,6 +4,7 @@ import moment from 'moment';
 import DatePicker from 'react-datetime';
 import 'react-datetime/css/react-datetime.css';
 import { useState } from 'react';
+import Swal from 'sweetalert2';
 
 const customStyles = {
     content: {
@@ -24,17 +25,59 @@ const CalendarModal = () => {
     const [dateStart, setDateStart] = useState(now.toDate());
     const [dateEnd, setDateEnd] = useState(nowPlus1.toDate());
 
+    const [formValues, setFormValues] = useState({
+        title: 'Evento',
+        notes: '',
+        start: now.toDate(),
+        end: nowPlus1.toDate(),
+    });
+    const [titleValid, setTitleValid] = useState(true)
+
+    const { title, notes, start, end } = formValues;
+
+    const handleInputChange = ({ target }) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value,
+        });
+    }
+
     const closeModal = () => {
 
     };
 
     const handleStartDateChange = (date) => {
         setDateStart(date);
+        setFormValues({
+            ...formValues,
+            start: date,
+        });
     };
 
     const handleEndtDateChange = (date) => {
         setDateEnd(date);
+        setFormValues({
+            ...formValues,
+            end: date,
+        });
     };
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        const momentStart = moment(start);
+        const momentEnd = moment(end);
+
+        if (momentStart.isSameOrAfter(momentEnd)) {
+            return Swal.fire('Error', 'La fecha de final debe ser mayor a la fecha de inicio', 'error');
+        }
+
+        if (title.trim().length < 2) {
+            return setTitleValid(false);
+        }
+
+        setTitleValid(true)
+        closeModal();
+    }
 
     const yesterday = moment(dateStart).subtract(1, 'day')
     const valid = (current) => {
@@ -50,9 +93,11 @@ const CalendarModal = () => {
             className="modal"
             overlayClassName="modal-fondo"
         >
-            <h1> Nuevo evento </h1>
-            <hr />
-            <form className="container">
+            <h1 className="title-modal"> Nuevo evento </h1>
+            <hr className="separador" />
+            <form
+                onSubmit={handleSubmitForm}
+                className="container">
                 <div className="form-group">
                     <label>Fecha y hora inicio</label>
                     <DatePicker
@@ -85,15 +130,17 @@ const CalendarModal = () => {
                     />
                 </div>
 
-                <hr />
+                <hr className="separador" />
                 <div className="form-group">
                     <label>Titulo y notas</label>
                     <input
                         type="text"
-                        className="form-control"
+                        className={`form-control ${!titleValid && 'is-invalid'}`}
                         placeholder="Título del evento"
                         name="title"
                         autoComplete="off"
+                        value={title}
+                        onChange={handleInputChange}
                     />
                     <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
                 </div>
@@ -105,6 +152,8 @@ const CalendarModal = () => {
                         placeholder="Notas"
                         rows="5"
                         name="notes"
+                        value={notes}
+                        onChange={handleInputChange}
                     ></textarea>
                     <small id="emailHelp" className="form-text text-muted">Información adicional</small>
                 </div>
